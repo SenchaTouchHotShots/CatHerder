0aac1f864569ed2df26df0257cef89ae6b14daa6
Bryan P Johnson <bryan@12ftguru.com>
2012-07-18 14:56:27 -0400

    cleanup

--- api/category.php
+++ api/category.php
@@ -1,8 +1,43 @@
 <?PHP
 
-  $test = array(
-    array('categoryID' => 2, 'name' => 'Category 2', 'itemID' => 1)
-  );
+$test = array(
+  array(
+    'categoryID' => 1,
+    'name' => 'Category 1',
+    'items' => array(
+      array(
+	'itemID' => 2,
+	'name' => 'Test Item 2',
+	'description' => 'Lorem Ipsum',
+	'price' => 2.00,
+	'photoURL' => 'http://placekitten.com/400/300',
+	'categoryID' => 1	
+      )
+    )
+  ),
+  array(
+    'categoryID' => 2,
+    'name' => 'Category 2',
+    'items' => array(
+      array(
+	'itemID' => 1,
+	'name' => 'Test Item 1',
+	'description' => 'Lorem Ipsum',
+	'price' => 1.00,
+	'photoURL' => 'http://placekitten.com/200/300',
+	'categoryID' => 2
+      ),
+      array(
+	'itemID' => 3,
+	'name' => 'Test Item 3',
+	'description' => 'Lorem Ipsum',
+	'price' => 3.50,
+	'photoURL' => 'http://placekitten.com/200/200',
+	'categoryID' => 2
+      )
+    )
+  ) 
+);
 
 
 echo json_encode($test);
--- api/item.php
+++ api/item.php
@@ -1,14 +1,38 @@
 <?PHP
 
 $test = array(
-  'itemID' => 1,
-  'name' => 'Test Item 1',
-  'description' => 'Lorem Ipsum',
-  'price' => 1.00,
-  'photoURL' => 'http://placekitten.com/200/300',
-  'categoryID' => 2,
-  'category' => array(
-    'categoryID' => 2, 'name' => 'Category 2', 'itemID' => 1
+  array(
+    'itemID' => 1,
+    'name' => 'Test Item 1',
+    'description' => 'Lorem Ipsum',
+    'price' => 1.00,
+    'photoURL' => 'http://placekitten.com/200/300',
+    'categoryID' => 2,
+    'category' => array(
+      'categoryID' => 2, 'name' => 'Category 2', 'itemID' => 1
+    )
+  ),
+  array(
+    'itemID' => 2,
+    'name' => 'Test Item 2',
+    'description' => 'Lorem Ipsum',
+    'price' => 2.00,
+    'photoURL' => 'http://placekitten.com/400/300',
+    'categoryID' => 1,
+    'category' => array(
+      'categoryID' => 1, 'name' => 'Category 1', 'itemID' => 2
+    )
+  ),
+  array(
+    'itemID' => 3,
+    'name' => 'Test Item 3',
+    'description' => 'Lorem Ipsum',
+    'price' => 3.50,
+    'photoURL' => 'http://placekitten.com/200/200',
+    'categoryID' => 2,
+    'category' => array(
+      'categoryID' => 2, 'name' => 'Category 2', 'itemID' => 3
+    )
   )
 );
 
