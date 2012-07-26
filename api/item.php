542446de2923fd2f82822cc25737fef342d76542
Bryan P Johnson <bryan@12ftguru.com>
2012-07-26 13:45:25 -0400

    initial functional api

--- api/item.php
+++ api/item.php
@@ -1,39 +1,109 @@
 <?PHP
 
-$test = array(
-  array(
-    'itemID' => 1,
-    'name' => 'Test Item 1',
-    'description' => 'Lorem Ipsum',
-    'price' => 1.00,
-    'photoURL' => 'http://placekitten.com/200/300',
-    'categoryID' => 1,
-    'category' => array(
-      'categoryID' => 1, 'name' => 'Category 1', 'itemID' => 1
-    )
-  ),
-  array(
-    'itemID' => 2,
-    'name' => 'Test Item 2',
-    'description' => 'Lorem Ipsum',
-    'price' => 2.00,
-    'photoURL' => 'http://placekitten.com/400/300',
-    'categoryID' => 2,
-    'category' => array(
-      'categoryID' => 2, 'name' => 'Category 2', 'itemID' => 2
-    )
-  ),
-  array(
-    'itemID' => 3,
-    'name' => 'Test Item 3',
-    'description' => 'Lorem Ipsum',
-    'price' => 3.50,
-    'photoURL' => 'http://placekitten.com/200/200',
-    'categoryID' => 1,
-    'category' => array(
-      'categoryID' => 1, 'name' => 'Category 1', 'itemID' => 3
-    )
-  )
-);
 
-echo json_encode($test);
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
+
+include_once 'dbSetup.inc';
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case "GET":
+    doGet();
+    break;
+case "POST":
+    doPost();
+    break;
+case "PUT":
+    doPut();
+    break;
+case "DELETE":
+    doDelete();
+    break;
+default:
+    doGet();
+}
+
+function doGet() {
+    /* When a get is performed, we are either getting one by ID in the url, or
+       many by sending along variables in the request string. */
+    $db = dbSetup();
+
+    if (preg_match('/item\/(\d+)[\/]*$/', $_SERVER['REQUEST_URI'], $matches)) {
+        /* We've got a single item to grab. */
+        $itemID = array($matches[1]); // execute expects an array.
+        $stmt = $db->prepare("select * from `items` where itemID = ?");
+        if (is_object($stmt) && $stmt->execute($itemID)) {
+            /* We only asked for one. */
+            $row = $stmt->fetch();
+            $row['category'] = getCategory($row['categoryID'], $db);
+            doJson($row);
+        } else {
+            doJson(array(
+                'success' => FALSE,
+                'error' => $stmt->errorInfo()
+            ));
+        }
+    } else {
+        $data = array();
+        $categories = array();
+        $filters = json_decode($_GET['filter'], TRUE);
+        $start = intval($_GET['start']);
+        $limit = intval($_GET['limit']);
+        /* For simplicity, just use one filter */
+        $filterColumn = $filters[0]['property'];
+        $filterValue = $filters[0]['value'];
+        $sql = "select * from `items`";
+        if (!is_null($filterValue) && $filterValue != 'null' && $filterValue != "") {
+            $sql .= " where `$filterColumn` = '$filterValue'";
+        }
+        if ($limit > 0) {
+            $sql .= " limit $start,$limit";
+        }
+        foreach ($db->query($sql) as $row) {
+            /* Only fetch categories once. */
+            if (!isset($categories[$row['categoryID']])) {
+                $categories[$row['categoryID']] = getCategory($row['categoryID'], $db);
+            }
+            $row['category'] = $categories[$row['categoryID']];
+
+            $data[] = $row;
+        }
+        
+        doJson($data);
+    }
+}
+
+function doPost() {
+
+}
+
+function doPut() {
+
+}
+
+function doDelete() {
+
+}
+
+function doJson($data) {
+    
+    echo json_encode($data);
+    
+}
+
+function getCategory($id, $db) {
+    static $categoryStmt = NULL;
+
+    if (is_null($categoryStmt)) {
+        $categoryStmt = $db->prepare("select * from `categories` where categoryID = ?");
+    }
+
+    $categoryID = array($id);
+    $category = array();
+    
+    if (is_object($categoryStmt) && $categoryStmt->execute($categoryID)) {
+        $category = $categoryStmt->fetch();
+    }
+    return $category;
+
+}
